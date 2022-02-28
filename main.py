a290e88819ae2940cf1b48520eea3cbfd98275ae
MilkMare <76255581+MilkMare@users.noreply.github.com>
2022-02-28 21:40:19 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -3,21 +3,19 @@ from telegram.ext import Updater
 from handlers import setup_dispatcher
 from settings import TELEGRAM_TOKEN, HEROKU_APP_NAME, PORT
 
-# Setup bot handlers
 updater = Updater(TELEGRAM_TOKEN)
 
 dp = updater.dispatcher
 dp = setup_dispatcher(dp)
 
-# Run bot
-if HEROKU_APP_NAME is None:  # pooling mode
+if HEROKU_APP_NAME is None:
     print("Can't detect 'HEROKU_APP_NAME' env. Running bot in pooling mode.")
     print("Note: this is not a great way to deploy the bot in Heroku.")
 
     updater.start_polling()
     updater.idle()
 
-else:  # webhook mode
+else:
     print(f"Running bot in webhook mode. Make sure that this url is correct: https://{HEROKU_APP_NAME}.herokuapp.com/")
     updater.start_webhook(
         listen="0.0.0.0",
