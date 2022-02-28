8585cd4a84474c0ee54d33349fb118336581d0ba
MilkMare <76255581+MilkMare@users.noreply.github.com>
2022-02-28 14:28:54 +0500

    Add files via upload

--- handlers.py
+++ handlers.py
@@ -12,7 +12,7 @@ def start(update, context):
     context.bot.send_message(
         chat_id=TELEGRAM_SUPPORT_CHAT_ID,
         text=f"""
-Пользователь присоеденился к боту {user_info}.
+Пользователь запустил бота {message.from_user.mention}.
         """,
     )
 
@@ -22,7 +22,7 @@ def forward_to_chat(update, context):
         'message_id': 5, 
         'date': 1605106546, 
         'chat': {'id': 49820636, 'type': 'private', 'username': 'danokhlopkov', 'first_name': 'Daniil', 'last_name': 'Okhlopkov'}, 
-        'text': 'TEST QOO', 'entities': [], 'caption_entities': [], 'photo': [], 'new_chat_members': [], 'new_chat_photo': [], 'delete_chat_photo': False, 'group_chat_created': true, 'supergroup_chat_created': False, 'channel_chat_created': False, 
+        'text': 'TEST QOO', 'entities': [], 'caption_entities': [], 'photo': [], 'new_chat_members': [], 'new_chat_photo': [], 'delete_chat_photo': False, 'group_chat_created': False, 'supergroup_chat_created': False, 'channel_chat_created': False, 
         'from': {'id': 49820636, 'first_name': 'Daniil', 'is_bot': False, 'last_name': 'Okhlopkov', 'username': 'danokhlopkov', 'language_code': 'en'}
     }"""
     forwarded = update.message.forward(chat_id=TELEGRAM_SUPPORT_CHAT_ID)
