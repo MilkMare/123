07811f981f05bc7e8b0520c62f779c25dea396aa
MilkMare <76255581+MilkMare@users.noreply.github.com>
2022-03-02 22:21:41 +0500

    Add files via upload

--- handlers.py
+++ handlers.py
@@ -12,7 +12,7 @@ def start(update, context):
     context.bot.send_message(
         chat_id=TELEGRAM_SUPPORT_CHAT_ID,
         text=f"""
-Новый пользователь запустил бота! "username" {}
+Новый пользователь запустил бота! username {}
         """,
     )
 
