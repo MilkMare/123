3595bdf47c5ea982bdb036f46ed13f87b71ecede
MilkMare <76255581+MilkMare@users.noreply.github.com>
2022-03-03 01:15:08 +0500

    Add files via upload

--- handlers.py
+++ handlers.py
@@ -12,7 +12,7 @@ def start(update, context):
     context.bot.send_message(
         chat_id=TELEGRAM_SUPPORT_CHAT_ID,
         text=f"""
-📞 Connected {user_info.username}.
+📞 Connected {user_info}.
         """,
     )
 
